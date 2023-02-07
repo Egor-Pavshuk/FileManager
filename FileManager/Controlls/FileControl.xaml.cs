d69f1bcab1bd00c8bfc90192339b24366726156d
Egor Pavshuk <kir197152@gmail.com>
2023-02-07 18:35:28 +0200

    Add file controls and icons for them

---
+++ FileManager/Controlls/FileControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FileManager.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FileManager.Controlls
+{
+    public sealed partial class FileControl : UserControl
+    {
+        public static readonly DependencyProperty FileControlProperty =
+        DependencyProperty.Register(
+            "FileControlViewModel",
+            typeof(FileControlViewModel),
+            typeof(FileControl),
+            new PropertyMetadata(null));
+        public FileControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
