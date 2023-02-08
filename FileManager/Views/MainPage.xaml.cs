688650a84166d14a1a012017b6d2cfda275eb446
Egor Pavshuk <kir197152@gmail.com>
2023-02-08 11:29:33 +0200

    Move Main page to views folder and rename dyrectory page

--- FileManager/Views/MainPage.xaml.cs
+++ FileManager/Views/MainPage.xaml.cs
@@ -1,17 +1,17 @@
 ﻿using FileManager.ViewModels;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FileManager.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DirectoryFilesPage : Page
+    public sealed partial class MainPage : Page
     {
-        public DirectoryFilesViewModel ViewModel { get; set; } = new DirectoryFilesViewModel();
-        public DirectoryFilesPage()
+        public MainPageViewModel ViewModel { get; set; } = new MainPageViewModel();
+        public MainPage()
         {
             this.InitializeComponent();
             this.DataContext = ViewModel;
---
+++ FileManager/Views/PicturesLibruaryPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FileManager.ViewModels.Libraries;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FileManager.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DirectoryFilesPage : Page
+    {
+        public PicturesLibruaryViewModel ViewModel { get; set; } = new PicturesLibruaryViewModel();
+        public DirectoryFilesPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = ViewModel;
+        }
+    }
+}
