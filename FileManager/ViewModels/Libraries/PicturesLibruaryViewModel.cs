764914a84f56a9db3d3fbfd3eade7a6b10b1d273
Egor Pavshuk <kir197152@gmail.com>
2023-02-08 11:30:49 +0200

    Make inheritance from base class for pictures library

---
+++ FileManager/ViewModels/Libraries/PicturesLibruaryViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace FileManager.ViewModels.Libraries
+{
+    public class PicturesLibruaryViewModel : LibrariesBaseViewModel
+    {
+        public PicturesLibruaryViewModel()
+        {
+            currentFolder = KnownFolders.PicturesLibrary;
+            CurrentPath = currentFolder.Path;
+            IsBackButtonAvailable = false;
+            _ = GetItemsAsync();
+        }
+        public override async void GetParent()
+        {
+            var newCurrentFolder = await currentFolder.GetParentAsync();
+            if (await newCurrentFolder.GetParentAsync() is null)
+            {
+                IsBackButtonAvailable = false;
+            }
+            currentFolder = newCurrentFolder;
+            CurrentPath = newCurrentFolder.Path;
+
+            StorageItems = await newCurrentFolder.GetFoldersAsync();
+            await GetItemsAsync();
+        }
+
+        public override async void OpenFolder(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            IsBackButtonAvailable = true;
+            var gridItems = sender as GridView;
+            if (!(gridItems.SelectedItem is FileControlViewModel selectedItem))
+            {
+                return;
+            }
+
+            CurrentPath = selectedItem.Path;
+            var newCurrentFolder = await StorageFolder.GetFolderFromPathAsync(CurrentPath);
+            currentFolder = newCurrentFolder;
+
+            StorageItems = await newCurrentFolder.GetFoldersAsync();
+            await GetItemsAsync();
+        }
+
+        protected override async Task GetItemsAsync()
+        {
+            if (currentFolder.IsEqual(KnownFolders.PicturesLibrary))
+            {
+                StorageItems = await currentFolder.GetItemsAsync();
+            }
+
+            List<FileControlViewModel> fileControls = new List<FileControlViewModel>();
+            foreach (var item in StorageItems)
+            {
+                var fileControl = new FileControlViewModel() { Image = "/Images/Folder.jpg", DisplayName = item.Name, Path = item.Path };
+                fileControls.Add(fileControl);
+            }
+
+            IReadOnlyList<StorageFile> storageFiles = await currentFolder.GetFilesAsync();
+            foreach (var item in storageFiles)
+            {
+                var viewModel = new FileControlViewModel() { Image = "/Images/File.png", DisplayName = item.Name, Path = item.Path };
+                fileControls.Add(viewModel);
+            }
+            StorageFiles = fileControls;
+        }
+    }
+}
