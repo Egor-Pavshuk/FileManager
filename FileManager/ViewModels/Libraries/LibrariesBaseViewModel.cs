3a77ac44f29f024351260508744741b5f6aead3b
Egor Pavshuk <kir197152@gmail.com>
2023-02-08 11:27:34 +0200

    Create abstract base class for libraries

---
+++ FileManager/ViewModels/Libraries/LibrariesBaseViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Input;
+
+namespace FileManager.ViewModels.Libraries
+{
+    public abstract class LibrariesBaseViewModel : INotifyPropertyChanged
+    {
+        private bool isBackButtonAvailable;
+        private FileControlViewModel selectedGridItem;
+        private IReadOnlyList<IStorageItem> storageItems;
+        private List<FileControlViewModel> storageFiles;
+        private string currentPath;
+        protected StorageFolder currentFolder;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+        public bool IsBackButtonAvailable
+        {
+            get => isBackButtonAvailable;
+            set
+            {
+                if (isBackButtonAvailable != value)
+                {
+                    isBackButtonAvailable = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public FileControlViewModel SelectedItem
+        {
+            get => selectedGridItem;
+            set
+            {
+                if (selectedGridItem != value)
+                {
+                    selectedGridItem = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public IReadOnlyList<IStorageItem> StorageItems
+        {
+            get => storageItems;
+            set
+            {
+                if (storageItems != value)
+                {
+                    storageItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public List<FileControlViewModel> StorageFiles
+        {
+            get => storageFiles;
+            set
+            {
+                if (storageFiles != value)
+                {
+                    storageFiles = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string CurrentPath
+        {
+            get => currentPath;
+            set
+            {
+                if (currentPath != value)
+                {
+                    currentPath = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public abstract void GetParent();
+        public abstract void OpenFolder(object sender, DoubleTappedRoutedEventArgs e);
+
+        protected virtual async Task GetItemsAsync()
+        {
+            if (currentFolder.IsEqual(KnownFolders.PicturesLibrary))
+            {
+                StorageItems = await currentFolder.GetItemsAsync();
+            }
+
+            List<FileControlViewModel> fileControls = new List<FileControlViewModel>();
+            foreach (var item in StorageItems)
+            {
+                var fileControl = new FileControlViewModel() { Image = "/Images/Folder.jpg", DisplayName = item.Name, Path = item.Path };
+                fileControls.Add(fileControl);
+            }
+
+            IReadOnlyList<StorageFile> storageFiles = await currentFolder.GetFilesAsync();
+            foreach (var item in storageFiles)
+            {
+                var viewModel = new FileControlViewModel() { Image = "/Images/File.png", DisplayName = item.Name, Path = item.Path };
+                fileControls.Add(viewModel);
+            }
+            StorageFiles = fileControls;
+        }
+    }
+}
