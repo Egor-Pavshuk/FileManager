da36988651991eec1d75559424bfaa5378c02028
Egor Pavshuk <kir197152@gmail.com>
2023-02-20 10:26:30 +0200

    Add validation

---
+++ FileManager/Validation/ItemNameValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FileManager.Validation
+{
+    public static class ItemNameValidation
+    {
+        private static readonly Regex template = new Regex(@"^[a-zA-Z0-9 ]+$");
+        public static bool Validate(string name)
+        {
+            return template.IsMatch(name);
+        }
+    }
+}
