cf86772d6f5d79d64f75f120a03f8f873230377e
Egor Pavshuk <kir197152@gmail.com>
2023-02-20 11:43:39 +0200

    Change validation template

--- FileManager/Validation/ItemNameValidation.cs
+++ FileManager/Validation/ItemNameValidation.cs
@@ -9,7 +9,7 @@ namespace FileManager.Validation
 {
     public static class ItemNameValidation
     {
-        private static readonly Regex template = new Regex(@"^[a-zA-Z0-9 ]+$");
+        private static readonly Regex template = new Regex(@"^[a-zA-Z0-9 _.]+$");
         public static bool Validate(string name)
         {
             return template.IsMatch(name);
