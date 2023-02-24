0a921cda0d8deacf861ee6dee3bf96945cfc9e69
Egor Pavshuk <kir197152@gmail.com>
2023-02-24 16:44:55 +0200

    Add view model locator

---
+++ FileManager/VMLocator/ViewModelLocator.cs
@@ -0,0 +1,66 @@
+﻿using FileManager.ViewModels;
+using System;
+using System.Globalization;
+using System.Reflection;
+using Windows.UI.Xaml;
+
+namespace FileManager.VMLocator
+{
+    public static class ViewModelLocator
+    {
+        public static DependencyProperty AutoWireViewModelProperty = DependencyProperty.RegisterAttached("AutoWireViewModel", typeof(bool),
+        typeof(ViewModelLocator), new PropertyMetadata(false, AutoWireViewModelChanged));
+
+        public static bool GetAutoWireViewModel(UIElement element)
+        {
+            return (bool)element?.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(UIElement element, bool value)
+        {
+            element?.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        private static void AutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                Bind(d);
+            }
+        }
+
+        private static void Bind(DependencyObject view)
+        {
+            if (view is FrameworkElement frameworkElement)
+            {
+                var viewModelType = FindViewModel(frameworkElement.GetType());
+                if (viewModelType != typeof(FileControlViewModel))
+                {
+                    frameworkElement.DataContext = Activator.CreateInstance(viewModelType);
+                }
+            }
+        }
+
+        private static Type FindViewModel(Type viewType)
+        {
+            string viewName = string.Empty;
+
+            if (viewType.FullName.EndsWith("Page"))
+            {
+                viewName = viewType.FullName
+                    .Replace("Page", string.Empty, StringComparison.Ordinal)
+                    .Replace("Views", "ViewModels", StringComparison.Ordinal);
+            }
+            else if (viewType.FullName.EndsWith("Control"))
+            {
+                viewName = viewType.FullName
+                    .Replace("Controlls", "ViewModels", StringComparison.Ordinal);
+            }
+
+            var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}ViewModel, {1}", viewName, viewAssemblyName);
+
+            return Type.GetType(viewModelName);
+        }
+    }
+}
