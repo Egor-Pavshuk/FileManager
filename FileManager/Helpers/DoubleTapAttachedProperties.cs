d710703d01199abcb034c4b98bfceb892675ab54
Egor Pavshuk <kir197152@gmail.com>
2023-04-06 15:01:51 +0300

    Move extensions to Helpers folder

--- FileManager/Helpers/DoubleTapAttachedProperties.cs
+++ FileManager/Helpers/DoubleTapAttachedProperties.cs
@@ -3,7 +3,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
-namespace FileManager.VMLocator
+namespace FileManager.Helpers
 {
     public static class DoubleTapAttachedProperties
     {
