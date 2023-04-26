7a28c7ccaa5e57dade58dc2e7c8c866b7df5296f
Egor-Pavshuk <kir197152@gmail.com>
2023-04-26 15:39:07 +0300

    Move constants from BatteryControlViewModel to Enums

---
+++ FileManager.Helpers/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Helpers
+{
+    public enum Enums
+    {
+         FullBattery = 100,
+         BitDischargedBattery = 76,
+         HalfBattery = 51,
+         ThirdBattery = 31,
+         LowBattery = 21
+    }
+}
